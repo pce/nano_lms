09b37edcce12e07b65d91e9c3e750d1aac210074
pce <info@pc-e.org>
2020-05-27 23:03:23 +0200

    cs

--- src/components/Events/EventsShowPage.js
+++ src/components/Events/EventsShowPage.js
@@ -22,49 +22,45 @@ function EventsShowPage(props) {
     })
   }, [props.match.params.id]);
 
- const handleEditEvent = (id) => {
-  props.history.push({
-   pathname: `/events/${id}/edit`
-  });
- };
+  const handleEditEvent = (id) => {
+    props.history.push({
+      pathname: `/events/${id}/edit`
+    });
+  };
 
- const handleDeleteEvent = (id) => {
-  // const event = { prod_name: data.prod_name, prod_desc: data.prod_desc, prod_price: parseInt(data.prod_price) };
-  deleteEvent(id)
-   .then((result) => {
-    setIsLoading(false);
-    props.history.push('/events')
-   }).catch((error) => setIsLoading(false));
- };
+  const handleDeleteEvent = (id) => {
+    // const event = { prod_name: data.prod_name, prod_desc: data.prod_desc, prod_price: parseInt(data.prod_price) };
+    deleteEvent(id).then((result) => {
+      setIsLoading(false);
+      props.history.push('/events')
+    }).catch((error) => setIsLoading(false));
+  };
 
- return (
+  return (
     <Container style={{marginTop:'2em'}}>
       <Row>
         <Col>
    {((isLoading) && (<Spinner animation="border" role="status">
     <span className="sr-only">Loading...</span>
    </Spinner>)) || (
-   <Jumbotron>
-    <h2>{data.event.title}</h2>
-    <p>{data.event.start}</p>
-    <p>{data.event.end}</p>
+        <Jumbotron>
+          <h2>{data.event.title}</h2>
+          <p>{data.event.start}</p>
+          <p>{data.event.end}</p>
+        <p>{formatDateRange(data.event.start, data.event.end)}</p>
+          <p>{((data.event.course_id) && data.event.course.title)}</p>
 
-   <p>{formatDateRange(data.event.start, data.event.end)}</p>
-
-    <p>{((data.event.course_id) && data.event.course.title)}</p>
-
-    <p>
-      <br/>
-      <Button type="button" variant="secondary" onClick={() => { props.history.goBack() }}>Cancel</Button>&nbsp;
-
-     <Button type="button" variant="primary" onClick={() => { handleEditEvent(data.event.id) }}>Edit</Button>&nbsp;
-     <Button type="button" variant="danger" onClick={() => { if (window.confirm('Delete?')) handleDeleteEvent(data.event.id) }}>Delete</Button>
-    </p>
-   </Jumbotron>)}
-  </Col>
-  </Row>
-  </Container>
- );
+        <p>
+          <br/>
+          <Button type="button" variant="secondary" onClick={() => { props.history.goBack() }}>Cancel</Button>&nbsp;
+          <Button type="button" variant="primary" onClick={() => { handleEditEvent(data.event.id) }}>Edit</Button>&nbsp;
+          <Button type="button" variant="danger" onClick={() => { if (window.confirm('Delete?')) handleDeleteEvent(data.event.id) }}>Delete</Button>
+        </p>
+         </Jumbotron>)}
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default withRouter(EventsShowPage);
